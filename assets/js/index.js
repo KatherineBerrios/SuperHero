b29ac60f54cc521a68c30765ff297d73d17190ae
berkathy <katherine.berrios.garcia@gmail.com>
2024-03-06 01:09:49 -0300

    Modificaciones en index.html e index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,26 @@ $(document).ready(function () {
 
           const apiKey = "4905856019427443";
           const apiUrl = `https://superheroapi.com/api.php/${apiKey}/${searchTerm}`;
-        }
+        })
+
+        $('.boton').on('click',function(){
+              $.ajax({
+                type: "GET",
+                url: "https://superheroapi.com/api.php/4905856019427443",
+                dataType: "json",
+                success: function (datosApi) {
+                  $(".text").text(JSON.stringify(datosApi));
+                  datosApi.forEach((element) => {
+                    $(".resultado").append(`<p>${element.id}-
+                ${element.name}-${element.username}-${element.email}-
+                ${element.phone}</p>`);
+                  });
+                },
+                error: function (error) {
+                  console.log(error);
+                },
+              });
+});
 
           $.ajax({
             type:"GET",
