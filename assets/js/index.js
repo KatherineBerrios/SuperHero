644abdc99d25baedb75f3c2cfe9b6c1a7f251482
berkathy <katherine.berrios.garcia@gmail.com>
2024-05-13 00:38:08 -0400

    Cambios en index.js, index.html, creación de README.md

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,41 +1,31 @@
+// Esta prueba no esta completa
 $(document).ready(function () {
-        $("form").submit(function (event) {
-          event.preventDefault();
-          const searchTerm = $("#superhero").val().trim();
-          if (!searchTerm.match(/^\d+$/)) {
-            alert("Por favor, ingresa solo números.");
-            return;
-          }
+  $("form").submit(function (event) {
+    event.preventDefault();
+    const searchTerm = $("#superhero").val().trim();
+    if (!searchTerm.match(/^\d+$/)) {
+      alert("Por favor, ingresa solo números.");
+      return;
+    }
 
-          const apiKey = "4905856019427443";
-          const apiUrl = `https://superheroapi.com/api.php/${apiKey}/${searchTerm}`;
-        })
+    // const apiKey = "4905856019427443";
+    const apiUrl = `https://superheroapi.com/api.php/4905856019427443/${searchTerm}`;
 
-        $('.boton').on('click',function(){
-              $.ajax({
-                type: "GET",
-                url: "https://superheroapi.com/api.php/4905856019427443",
-                dataType: "json",
-                success: function (datosApi) {
-                  $(".text").text(JSON.stringify(datosApi));
-                  datosApi.forEach((element) => {
-                    $(".resultado").append(`<p>${element.id}-
-                ${element.name}-${element.username}-${element.email}-
-                ${element.phone}</p>`);
-                  });
-                },
-                error: function (error) {
-                  console.log(error);
-                },
-              });
+    $.ajax({
+      type: "GET",
+      url: apiUrl,
+      dataType: "json",
+      success: function (datosApi) {
+        datosApi.forEach((element) => {
+          $(".resultado").text(JSON.stringify(datosApi));
+          $(".resultado").append(
+            `<p>ID: ${element.id} - Nombre: ${element.name}</p>`
+          );
+        });
+      },
+      error: function (error) {
+        console.log(error);
+      },
+    });
+  });
 });
-
-          $.ajax({
-            type:"GET",
-            url:"https://superheroapi.com/api.php/4905856019427443",
-            success: function(data) {
-            $('.text').text(JSON.stringify(data));
-            },
-            dataType: 'json',
-          });
-      });
